37a70d162abba860e66d9edfecc64d27346d3411
Marco Stevanon <20814765+marcostevanon@users.noreply.github.com>
2026-01-07 16:50:26 +0100

    Update button mapping for plant entities in configuration

--- src/ha_launchpad/config.py
+++ src/ha_launchpad/config.py
@@ -78,8 +78,8 @@ BUTTON_MAP: Dict[int, str] = {
     47: "volume_up.media_player.nestmini7849",
 
     # plants
-    11: "plant.monstera",
-    12: "plant.pothos",
+    18: "plant.monstera",
+    19: "plant.pothos",
 }
 
 # Pads that should enter color-pick mode when pressed (keys from BUTTON_MAP)
